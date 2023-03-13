f879a6c09c304baafc67d0897f6cd7aacbcffeaf
islamavazovm <islamavazovm@gmail.com>
2023-03-13 16:13:41 +0600

    homework2

--- hero.py
+++ hero.py
@@ -22,3 +22,54 @@ class SuperHero:
 
 
 my_hero = SuperHero('Piter Parker', 'Spiderman', 'super spider', 100, 'piter spider man')
+
+class Tanoshero(SuperHero):
+    glove = 'glove'
+    def __init__(self,name, nickname, superpower, health_points, catchphrase,damage=False, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def double_health_points(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def fly(self):
+        return f'fly in the {self.fly}_phrase'
+
+class Darkside(SuperHero)
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage=False, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def double_health_points(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def fly(self):
+        return f'fly in the {self.fly}_phrase'
+
+thor = Tanoshero('thor', 'bog', 'grom', 200, 'хз', damage=300)
+cap = Darkside('cap', 'pon', 'hello', 150, 'ladno', damage=100)
+class Vilian(Tanoshero):
+    SuperHero.people = 'monster'
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+
+    def Gen_x(self):
+        pass
+
+    def crit(self, hero):
+       return self.damage ** 2
+
+NAme = Vilian('tanos', 'tanka', 'kamni', 900, 'pon')
+print(Vilian.crit(NAme, thor))
+print(Vilian.crit(NAme, cap))
+
+
+
+
+
+
